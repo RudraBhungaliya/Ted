be1be96deb2a1fccf576b77195e941f9f5efb2b2
Rudra Bhungaliya <133981939+RudraBhungaliya@users.noreply.github.com>
2026-04-19 14:26:41 +0530

    added middleware, multer, eventBus

---
+++ apps/api/src/lib/eventBus.ts
@@ -0,0 +1,14 @@
+type Client = (data : any) => void;
+let clients : Client[] = [];
+
+export const subscribe = (fn : Client) => {
+    clients.push(fn);
+};
+
+export const unsubscribe = (fn : Client) => {
+    clients = clients.filter(c => c !== fn);
+};
+
+export const publish = (data : any) => {
+    clients.forEach(fn => fn(data));
+}
